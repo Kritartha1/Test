b839e18ceabee8f2200fd30e1bc9c4ebd6b1c47c
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-06-13 18:49:17 +0530

    Create t12-codeforces-contest.java

---
+++ Test12/t12-codeforces-contest.java
@@ -0,0 +1,7 @@
+// Test 12:
+// 1: https://codeforces.com/contest/1697/problem/A    Done
+// 2: https://codeforces.com/contest/1697/problem/B
+// 3: https://codeforces.com/contest/1697/problem/C
+// 4: https://codeforces.com/contest/1697/problem/D
+// 5: https://codeforces.com/contest/1697/problem/E
+// 6: https://codeforces.com/contest/1697/problem/F
